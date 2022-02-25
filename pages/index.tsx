4961dc73bd16d9784aba63888bf28be9b07b9423
Khaue Poerner <khaue.p@grupovvale.local>
2022-02-24 22:37:05 -0300

    feature: mudanças home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 export default function Home() {
 	return (
 		<section className="mt-16 md:mt-32 max-w-screen-xl mx-auto px-5">
-			{/* <h2 className="text-5xl md:text-7xl font-bold md:w-2/3 text-gray-800 tracking-tighter">Transformando ideias em códigos excepcionais.</h2> */}
-			<h2 className="text-5xl md:text-7xl font-bold md:w-2/3 text-gray-800 tracking-tighter">Lorem ipsum dolor sit amet, consectetur.</h2>
-			<p className="text-xl md:w-2/3 text-gray-700 mt-8 md:mt-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consectetur fermentum velit, rutrum accumsan justo volutpat sit amet. Vivamus vitae viverra augue. Aliquam semper cursus felis, sed porttitor nulla imperdiet vitae.</p>
+			{/* <h2 className="text-5xl md:text-7xl font-bold md:w-2/3 text-gray-800 tracking-tighter">Passo o dia escrevendo código
+				<span className="rainbow-letters">
+					<span> c</span><span>o</span><span>l</span><span>o</span><span>r</span><span>i</span><span>d</span><span>o</span>
+				</span>.
+			</h2> */}
+			{/* <p className="text-xl md:w-2/3 text-gray-700 mt-8 md:mt-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consectetur fermentum velit, rutrum accumsan justo volutpat sit amet. Vivamus vitae viverra augue. Aliquam semper cursus felis, sed porttitor nulla imperdiet vitae.</p> */}
+			<h2 className="text-5xl md:text-7xl font-bold md:w-2/3 text-gray-800 tracking-tighter">Compartilhando códigos, ideias e processos.</h2>
+			<p className="text-xl md:w-2/3 text-gray-700 mt-8 md:mt-16">Quanto mais conhecimento consigo compartilhar, mais conhecimento consigo absorver. Dividir meu conhecimento é uma maneira de fortalecer o que já sei e ainda uma oportunidade de aprender.</p>
 		</section>
   	)
 }
